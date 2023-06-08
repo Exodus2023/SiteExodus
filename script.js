7c7032944c37fb078dc10c2ef5bbf0597dde723f
Lucas Oliveira <lucasbarbosadeoliveira95@gmail.com>
2023-06-08 17:57:02 -0300

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const projectDes = document.querySelector(".Pdesc-text");
+const projectText = "É um aplicativo voltado para pessoas que prestam serviços de beleza, para que elas possam organizarmelhor suas agendas, cadastrar(ou não) seus salões. Além disso os consumidores poderão agendar horário remotamente com apenas um click e a confirmação do profissional";
+
+projectDes.append(projectText)
+
+var width = window.innerWidth
+
+alert("tamanho = " + width)
+
+
+
 function redeSocial(rede){
     window.open(rede, "_blank");
 }
